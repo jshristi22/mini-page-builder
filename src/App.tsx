e2fd4717e1383a4aceb07ba5d87c5bd352d6f7df
jshristi22 <jshristi17@gmail.com>
2023-11-27 23:19:51 +0530

    implements page builder

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.module.scss'
+import Canvas from './canvas/canvas'
+import PageBuilderTools from './page_builder_tools/page_builder_tools'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+{/* <Canvas /> */}
+<PageBuilderTools />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/canvas/canvas.tsx
@@ -0,0 +1,12 @@
+
+import styles from './canvas.module.scss';
+
+function Canvas() {
+  return (
+    <div className={styles.canvasContainer}>
+      
+    </div>
+  )
+}
+
+export default Canvas
